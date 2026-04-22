06b22e773e2961927310d4500eaed06e1c97c692
Rehan <muhammadrehan302005@gmail.com>
2026-04-22 22:34:16 +0700

    Update LCD_Running_Text.ino

--- LCD_Running_Text/LCD_Running_Text.ino
+++ LCD_Running_Text/LCD_Running_Text.ino
@@ -1,23 +1,21 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// --- KONFIGURASI ---
-// Alamat I2C biasanya 0x27. Kalau ntar layar cuma nyala biru, 
-// coba ganti ke 0x3F. (16 = kolom, 2 = baris)
+// --- KONFIGURASI ---)
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
-// Teks berjalan lo, kasih spasi di depan & belakang biar muternya mulus
+// Teks berjalan
 String teksBerjalan = "         Semangat Kuliah di PVTE UMRI         ";
 
 void setup() {
   lcd.init();        // Inisialisasi LCD
-  lcd.backlight();   // Nyalain lampu latar biar gak gelap gulita
+  lcd.backlight();   // Nyalain lampu latar 
   
-  // Teks nangkring di baris atas
+  // Teks di baris atas
   lcd.setCursor(0, 0); 
   lcd.print("Kelompok Gacor");
   
-  // Teks pemanasan sebelum jalan
+  // Teks sebelum jalan
   lcd.setCursor(0, 1);
   lcd.print("Loading...");
   delay(2000); // Jeda 2 detik
